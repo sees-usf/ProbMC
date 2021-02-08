c8a2b1159be3887c8308be621f9c09d18cf011b2
LaureanoG <40482521+LaureanoG@users.noreply.github.com>
2021-02-08 11:25:17 -0500

    Add files via upload

---
+++ ic3/frame.py
@@ -0,0 +1,12 @@
+from z3 import *
+from models import *
+
+
+class frame:
+    def __init__(self, model, inputK):
+        self.clauses = model.getProperty(inputK)
+        self.k = inputK
+        self.T = model.getTransition(inputK)
+        self.P = model.getProperty(inputK)
+        self.PPrime = model.getProperty(inputK+1)
+        self.solver = Solver()
---
+++ ic3/main.py
@@ -0,0 +1,148 @@
+from models import *
+from frame import *
+from z3 import *
+
+
+# Runs IC3
+def ic3(model):
+    # Input: model
+    # Output: Boolean representing counter example or not
+    # True = there is a counterexample; False =  There is not a counterexample
+
+    # INITIAL CHECKING
+    # Checking Sat(init ^ -P)
+    frame0 = frame(model, 0)
+    frame0.clauses = model.getInit()
+    initCheckSolver1 = Solver()
+    initCheckSolver1.add(Not(frame0.P))
+    initCheckSolver1.add(model.getInit())
+    if initCheckSolver1.check() == sat:
+        print("First Check failed", initCheckSolver1.model())
+        return True
+    else:
+        print('F0 -> P holds')
+
+    # Checking Sat(init ^ T ^ -P')
+    initCheckSolver2 = Solver()
+    initCheckSolver2.add(model.getInit())
+    initCheckSolver2.add(frame0.T)
+    initCheckSolver2.add(Not(frame0.PPrime))
+    if initCheckSolver2.check() == sat:
+        print("Second check failed", initCheckSolver2.model())
+        return True
+    else:
+        print('F0 /\ T -> P holds')
+
+    # INITIALIZATION FOR BACKWARDS SEARCH
+    prevFrame = frame0
+    currFrame = frame(model, 1)
+    frames = [frame0]
+    k = 1
+
+    # BACKWARDS SEARCH
+    i = 0  # Var for testing
+    while True:
+        print("\nCurr frame:", currFrame.k, "Prev Frame", prevFrame.k)
+        # Checks for a bad state
+        # Sat( Fk ^ T ^ -P')
+        badStateSolver = Solver()
+        badStateSolver.add(currFrame.clauses)
+        badStateSolver.add(currFrame.T)
+        badStateSolver.add(Not(currFrame.PPrime))
+
+        while badStateSolver.check() == sat:
+            sk = retrieveSk(currFrame, badStateSolver)
+            print("There is a bad state: ", sk)
+            result, frames = findCEX(sk, currFrame, frames, model)
+            if result:
+                return True
+            break
+
+        # If frames match, its the end of ic3
+        if prevFrame == currFrame:
+            return False
+
+        # PREPS FOR NEXT ITERATION
+        k += 1
+        frames.append(prevFrame)
+        prevFrame = currFrame
+        currFrame = frame(model, k)
+
+        # Controls loop for testing
+        i += 1
+        if i == 1:
+            break
+    pass
+
+
+# Retrieves a bad state s from Fk
+def retrieveSk(Fk, skSolver):
+    # Input: Transition relation ANDED w/ Not P, the current frame
+    # Output: Returns a bad state
+
+    model = skSolver.model()
+    print(skSolver.model())
+
+    a = Bool("a{0}".format(Fk.k))
+    b = Bool("b{0}".format(Fk.k))
+
+    # TODO needs to be reworked for larger models
+    if not model.eval(a) and not model.eval(b):
+        sk = And(Not(a), Not(b))
+    elif not model.eval(a):
+        sk = And(Not(a), b)
+    elif not model.eval(b):
+        sk = And(a, Not(b))
+    else:
+        sk = And(a, b)
+
+    print("The bad state is ", sk)
+    checker = Solver()
+    checker.add(sk)
+    checker.add(Fk.T)
+    checker.add(Not(Fk.P))
+
+    # TODO Figure out whats wrong
+    if checker.check() == unsat:
+        raise ValueError("Something wrong with sk")
+
+    return sk
+
+
+# Tries to find a counterexample
+def findCEX(sk, Fk, frames, model):
+    currSolver = Solver()
+
+    # We reached init
+    if Fk.k == 0:
+        currSolver.add(Implies(sk, Fk.clauses))
+        if currSolver.check():
+            return True, frames
+        else:
+            return False, frames
+    else:
+        # Finds a bad state in the previous frame
+        # (Fk-1 ^ T ^ sk')
+        currSolver.add(Fk.clauses)
+        currSolver.add(Fk.T)
+        currSolver.add()
+
+    return False, frames
+
+
+# Prints Result
+def printResult(result):
+    if isinstance(result, bool):
+        if result:
+            print("There is a counterexample")
+        else:
+            print("P is verified")
+    else:
+        print("Inconclusive")
+
+
+# Starts IC3
+print("**MODEL 1**")
+printResult(ic3(sampleModel1()))
+print("\n**MODEL 2**")
+printResult(ic3(sampleModel2()))
