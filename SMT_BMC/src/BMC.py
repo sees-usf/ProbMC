a172fcaf441f23dfc144c707d5c26673e9bd1b4e
Luke M <42702805+luke124125@users.noreply.github.com>
2021-08-14 12:09:11 -0400

    Add files via upload

--- SMT_BMC/src/BMC.py
+++ SMT_BMC/src/BMC.py
@@ -1,19 +1,30 @@
 """SMT Bounded Model Checker Class"""
 
 from z3 import *
+from Graph import *
 import importlib
 
 class BMC:
 
-    def __init__(self, path_length, model, property_prob):
+    def __init__(self, path_length, model, property_prob, debug):
         """
         Initializes variables and finds the total probability of the property by looping through all possible steps
         and calling various functions within this class. Prints out whether or not a counterexample was found or not.
         """
+        if(debug == 1): # Clears txt file if results are desired.
+            f = open("result.txt", "a")
+            f.truncate(0)
+            f.close()
         self.path_length = path_length # Max number of steps that can be taken to find the criteria-fitting cx
         self.model = importlib.import_module(model) # import the model's .py file
         self.property_prob = property_prob # The probability that must be met to consider a cx as found
         self.reached_cx_list = [] # List of cx's already reached, including prior steps.
+        self.cx_list_asStrings = [] # List of cx's in the form of strings.(Only used when printing)
+        self.cx_list_probablities = [] # List of only the probabilities.(Only used when printing)
+        self.nodeList = [] # List of nodes.(Only used when printing)
+        self.priorValues = [] # Keeps track of prior counter examples.(Only used when printing)
+        self.T = 0 # Keeps track of T. (Only used when printing)
+        self.nodeCounter = 0 # Couting nodes for printing. (Only used when printing)
         self.solver = Solver() # Holds all the constraints that define a bounded model
         self.solver.add(self.model.GetInitialStates()) # Add initial states to the bounded model definition
         # Increase the path_length until a counterexample that meets property_prob is found
@@ -22,7 +33,7 @@ class BMC:
         for i in range(1, (self.path_length+1)):
             self.path_length = i
             self.PathEncoding(i)
-            prob_from_step = self.Check(i)
+            prob_from_step = self.Check(i, debug)
             print(i, prob_from_step)
             total_probability += prob_from_step
             if total_probability >= property_prob:
@@ -34,38 +45,44 @@ class BMC:
         print("Total probability: {}".format(total_probability))
 
 
-    def PathEncoding(self, i):
+    def PathEncoding(self, step):
         """
         Adds an encoding of the path with a length of path_length, which is incremented in __init__
         """
-        path = self.model.GetStep(i)
+        path = self.model.GetStep(step)
         self.solver.add(path)
-
-    def ExcludePath(self, cx_model):
+        
+    def ExcludePath(self, cx_model, debug):
         """
         Returns all found counterexamples to avoid recounting. The constraints are already complemented, so there 
         is no need to add NOT() before using AND() to merge the original model to the returned constraints.
         """
         # Creating a list of each variable's value to later merge together into the full counterexample
         cx_value_list = []
-        for declaration in cx_model.decls():
-            # print ("%s = %s" % (declaration.name(), cx_model[declaration]))
-            name = str(declaration.name())
-            if name.find('/0') != -1: #Avoiding '/0' in counter example (Appears when using '/' in model).
-                break;
-            if is_int_value(cx_model[declaration]):
-                cx_value_list.append(Int(declaration.name())==cx_model[declaration].as_long())
-            elif is_true(cx_model[declaration]) or is_false(cx_model[declaration]):
-                cx_value_list.append(Bool(declaration.name())==cx_model[declaration])
+        for z in self.model.GetTransitionProbStrings(): # For all the probabilities
+            self.cx_list_probablities.append(cx_model[z])
+        for z in self.model.GetStateVaribleStrings():
+            if is_int_value(cx_model[z]): # For Integer Values
+                cx_value_list.append(z == cx_model[z])
+                self.cx_list_asStrings.append(str(z)+" == "+str(cx_model[z]))
+            elif is_true(cx_model[z]) or is_false(cx_model[z]): # For Boolean Values
+                cx_value_list.append(z == cx_model[z])
+                self.cx_list_asStrings.append(str(z)+" == "+str(cx_model[z]))
             else:  # For Real Values
-                if(name.find('p.') == -1): #Avoid adding the probability to counter example
-                    cx_value_list.append(Real(declaration.name())==cx_model[declaration])
+                cx_value_list.append(z == cx_model[z])
+                self.cx_list_asStrings.append(str(z)+" == "+str(cx_model[z]))
         
         cx = Not(And(cx_value_list))  # Counterexample to begin avoiding
         self.reached_cx_list.append(cx)  # Add to list of all the counterexamples to avoid
+        
+        # Prints results to txt file
+        if(debug == 1):
+            self.Print()
+            self.cx_list_asStrings.clear()
+            
         return self.reached_cx_list
-
-    def Check(self, i):
+        
+    def Check(self, i, debug):
         """
         Finds all counterexample models given a path at its most recent step and returns the probability of the path occuring
         """
@@ -87,20 +104,78 @@ class BMC:
 
             # Find probability of the counterexample by multiplying all of the step's probabilities together
             probability = 1
-            for k in range(self.path_length+1):
-                for d in cx_model.decls():
-                    if d.name() == "p.{0}".format(k):
-                        numerator = float(cx_model[d].numerator_as_long())
-                        denominator = float(cx_model[d].denominator_as_long())
-                        probability *= numerator/denominator
+            for z in self.model.GetTransitionProbStrings():
+                numerator = float(cx_model[z].numerator_as_long())
+                denominator = float(cx_model[z].denominator_as_long())
+                probability *= numerator/denominator
             
-            self.solver.add(self.ExcludePath(cx_model))
+            self.solver.add(self.ExcludePath(cx_model, debug))
             total_probability += probability  # Adding the probability of each cx at the given pathlength
 
         # Runs when no more counterexamples can be found
         self.solver.pop() # Removes the property to replace with a fresh one later, when Check() is called again
         return total_probability
 
+    def Print(self):
+        """
+        Generates a txt file named "results.txt" that will contain the counter examples from the simulation.
+        """
+        graph = Graph() # Create graph data structure used for printing.
+        subString = self.cx_list_asStrings[0]
+        value = 0 # Keeps track of the step value to keep all the variables of the same step together.
+        i = 1 # used to index the loop.
+        firstRun = 1 # Used to distinguish the first run.
+        previousEdge = 0 # Keeps track of the previous edge.
+        
+        while(i < len(self.cx_list_asStrings)):
+            if(self.cx_list_asStrings[i].find("."+str(value)) != -1):
+                subString = subString + ", " + self.cx_list_asStrings[i] # Collecting all varaibles of the same step.
+                i = i + 1
+            else:
+                try: # Try and except used to search for the node to make sure it is not printed twice.
+                    x = self.priorValues.index(str(subString))
+                    if(firstRun): # The first run is just used to store the first edge as the next edge is not known yet.
+                        previousEdge = x
+                        firstRun = 0
+                    else:
+                        graph.addEdge("n"+str(previousEdge) + " " + ("n"+str(x)), str(self.cx_list_probablities[value])) # Adding edge.
+                        previousEdge = x
+                 
+                except:
+                    if(firstRun):
+                        graph.addVertex(("n"+str(self.nodeCounter)), subString) # Adding node
+                        previousEdge = self.nodeCounter
+                        firstRun = 0
+                    else:
+                        graph.addVertex(("n"+str(self.nodeCounter)), subString) # Adding node
+                        graph.addEdge("n"+str(previousEdge) + " " + ("n"+str(self.nodeCounter)), str(self.cx_list_probablities[value])) # Adding edge.
+                        previousEdge = self.nodeCounter
+                        
+                    self.nodeCounter = self.nodeCounter + 1
+                    self.priorValues.append(str(subString))
+                    
+                subString = self.cx_list_asStrings[i]
+                i = i + 1
+                value = value + 1
+               
+        try: # Adding the last node "T" which holds the property
+            x = self.priorValues.index(str(self.model.GetProperty(value)))
+            graph.addEdge("n"+str(previousEdge) + " " + ("T" + str(self.T)), -1) # Adding last edge.
+        except:
+            graph.addVertex("T" + str(self.T), subString)
+            self.priorValues.append(subString)
+            graph.addEdge("n"+str(previousEdge) + " " + ("T" + str(self.T)), -1) # Adding last edge.
+            self.T = self.T + 1
+            
+        string = graph.returnGraph() # Generate string
+            
+        f = open("result.txt", "a") # Write to the result.txt file.
+        f.write(string)
+        f.close()
+        
+        return
+        
     def GetCounterExample():
         pass
         # if counter example found, user can query to get the cx 
+        
--- SMT_BMC/src/main.py
+++ SMT_BMC/src/main.py
@@ -18,6 +18,12 @@ from BMC import *
 # Obtain information about the model to check and what to check for.
 example = "?"
 while example != "y" and example != "n":
+  debugMode = input("Would you like to print your counter examples? (y/n): ")
+  if debugMode == "y":
+    debug = 1
+  else:
+    debug = 0
+    
   example = input("Is your model in the examples folder or in the main src directory? (y/n): ")
   if example == "y":
     model = "examples." + input("Model name (exclude .py): ")
@@ -27,4 +33,4 @@ while example != "y" and example != "n":
 path_length = int(input("Provide a path length: "))
 property_prob = float(input("Provide a probability to reach: "))
 
-bmc = BMC(path_length, model, property_prob) # BMC initialization runs the SMT-BMC solver
+bmc = BMC(path_length, model, property_prob, debug) # BMC initialization runs the SMT-BMC solver
