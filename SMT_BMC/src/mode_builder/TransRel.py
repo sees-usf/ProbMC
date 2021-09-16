f08031663df7011bd6f86a5d5ee4f8aa44e5bc58
Luke M <42702805+luke124125@users.noreply.github.com>
2021-09-15 20:32:34 -0400

    Delete SMT_BMC directory

--- SMT_BMC/src/mode_builder/TransRel.py
+++
@@ -1,9 +0,0 @@
-#@ Class for building transition relations
-class TransRel:
-    def __init__(self, nxt_state_var, cur_state_def):
-        self.next_state_var = nxt_state_var
-        self.cur_state_def = cur_state_def
-        
-    def get_z3_encoding(self):
-        #@  to be filled
-        
--- SMT_BMC/trash/model.py
+++
@@ -1,7 +0,0 @@
-from z3 import *
-
-def GetStep():
-
-#model_info
-
-    return step
